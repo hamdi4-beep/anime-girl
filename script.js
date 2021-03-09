5579d5ab66c2c5129f548c739305de26395edd25
hamdi-beep4 <git@github.com>
2021-03-09 10:20:37 -0800

    improved animation and added music

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// glow animation mechanism
+
 const p = document.querySelector("p");
 const classNames = ["#F06", "#3bbced", "#777","#555","#3dc200",
                     "#9000ff", "#0ef", "#60f", "#f03", "#f60"];
@@ -19,9 +21,35 @@ for (let x=0; x<spans.length; x++) {
 
 spans.forEach(function(span, i) {
     span.classList.add(animationNames[i]);
-    console.log(i);
 });
 
+// rotate animation
+
 p.addEventListener("click", function(e) {
-    this.classList.add("rotate");
+    if (this.classList[0] != "rotate") this.classList.add("rotate");
+    else this.classList.remove("rotate");
 });
+
+// slideshow timer
+
+const images = ["https://i.pinimg.com/originals/35/8b/53/358b539b1fe285e32573de0a9cd15182.jpg",
+                "https://wallpaperaccess.com/full/158905.jpg",
+                "https://wallpaperaccess.com/full/2415294.jpg",
+                "https://images.hdqwalls.com/wallpapers/cyberpunk-2077-4k-2020-game-jd.jpg",
+                "https://images.wallpapersden.com/image/download/stray-game-cyberpunk_bGZoZ2WUmZqaraWkpJRmbmdlrWZlbWU.jpg"]
+let x = 0;
+
+setInterval(function() {
+    if (x != 5) {
+        document.body.style.backgroundImage = "url(" + images[x] + ")";
+        x++;
+    } else {
+        x = 0;
+    }
+}, 3000);
+
+// sound mechanism
+
+const audio = document.createElement("audio");
+audio.src = "https://vgmsite.com/soundtracks/persona-3-original-soundtrack/fmmwwmjr/1-12%20When%20The%20Moon%27s%20Reaching%20Out%20Stars.mp3";
+audio.play();
