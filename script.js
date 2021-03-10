13289b5b576df778056f60cf5cbb0db34fba01b6
hamdi-beep4 <git@github.com>
2021-03-10 09:37:08 -0800

    added a bar menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // glow animation mechanism
 
-const p = document.querySelector("p");
-const classNames = ["#F06",
+const p = document.querySelector(".name");
+const colors = ["#F06",
                     "#3bbced",
                     "#777",
                     "#555",
@@ -16,26 +16,30 @@ let str = p.textContent;
 const animationNames = ["glowA", "glowB"];
 
 str = str.replace(/\w/g, function(x, i) {
-    return "<span style='color:" + classNames[i] + "'>" + x + "</span>";
+    return "<span style='color:" + colors[i] + "'>" + x + "</span>";
 });
 
 p.innerHTML = str;
+
+// letters animation
 
 const spans = p.querySelectorAll("span");
 
 for (let x=0; x<spans.length; x++) {
     animationNames.push(animationNames[x]);
+    spans.forEach(function(span, i) {
+        span.classList.add(animationNames[i]);
+    });
 }
 
-spans.forEach(function(span, i) {
-    span.classList.add(animationNames[i]);
-});
+// welcome mechanism
 
-// fade animation
-
-p.addEventListener("click", function(e) {
-    if (this.style.backgroundColor != "transparent") this.style.backgroundColor = "transparent";
-    else this.style.backgroundColor = "#FFF";
+p.addEventListener("click", function() {
+    if (this.style.backgroundColor != "transparent") {
+        this.style.backgroundColor = "transparent";
+    } else {
+        this.style.backgroundColor = "#FFF";
+    }
 });
 
 // slideshow timer
@@ -46,27 +50,26 @@ const images = ["https://i.pinimg.com/originals/35/8b/53/358b539b1fe285e32573de0
                 "https://wallpaperaccess.com/full/2415294.jpg",
                 "https://images.hdqwalls.com/wallpapers/cyberpunk-2077-4k-2020-game-jd.jpg",
                 "https://images.wallpapersden.com/image/download/stray-game-cyberpunk_bGZoZ2WUmZqaraWkpJRmbmdlrWZlbWU.jpg",
-                "https://wallpapercave.com/wp/wp5392103.jpg"]
+                "https://wallpapercave.com/wp/wp5392103.jpg",
+                "https://wallpaperaccess.com/full/3311394.jpg"]
 let x = 0;
 
 setInterval(function() {
-    if (x != 5) {
+    if (x < images.length) {
         bg.style.backgroundImage = "url(" + images[x] + ")";
         x++;
     } else {
         x = 0;
     }
-}, 4000);
+}, 5000);
 
 // sound mechanism
 
 const audio = document.createElement("audio");
-audio.src = "https://vgmsite.com/soundtracks/persona-3-original-soundtrack/fmmwwmjr/1-12%20When%20The%20Moon%27s%20Reaching%20Out%20Stars.mp3";
+audio.src = "https://vgmsite.com/soundtracks/persona-3-original-soundtrack/kyyyndbh/2-05%20Living%20With%20Determination.mp3";
 audio.autoplay = "autoplay";
 
-bg.onclick = function() {
+document.querySelector(".menu").onclick = function() {
     if (audio.paused) audio.play();
     else audio.pause();
 }
-
-alert("Click anywhere on the background to play music.");
