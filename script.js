5c94702c3613145c1cca588a53f65ea0bec13575
hamdi-beep4 <git@github.com>
2021-03-30 11:48:35 -0700

    added a todo list

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-document.querySelector("ul").addEventListener("click", function(e) {
-    if (e.target.tagName == "LI") {
-        const target = e.target;
-        this.querySelectorAll("li").forEach(function(li) {
-            if (target == li) {
-                target.classList.add("current");
-            } else {
-                li.classList.remove("current");
-            }
-        })
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", e => {
+    e.preventDefault();
+    const todoDiv = document.createElement("div");
+    const newTodo = document.createElement("li");
+    const completedBtn = document.createElement("button");
+    const trash = document.createElement("button");
+
+    todoDiv.classList.add("todo");
+    newTodo.classList.add("todo-item");
+    completedBtn.classList.add("completed-btn");
+    trash.classList.add("trash-btn");
+    newTodo.textContent = todoInput.value;
+    todoDiv.appendChild(newTodo);
+    completedBtn.innerHTML = "<i class='fas fa-check'></i>";
+    trash.innerHTML = "<i class='fas fa-trash'></i>";
+    todoDiv.appendChild(completedBtn);
+    todoDiv.appendChild(trash);
+    todoList.appendChild(todoDiv);
+});
+
+todoList.addEventListener("click", e => {
+    const item = e.target;
+    if (item.classList[0] == "trash-btn") {
+        item.parentElement.remove();
     }
 });
---
+++ test.py
@@ -0,0 +1,2 @@
+name = "Hamdi"
+print(name)
